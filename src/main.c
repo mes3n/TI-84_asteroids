1821467d45f53a60c9715c8cc8c08fa6e742ef52
mes3n <skidmackan@gmail.com>
2022-04-09 23:24:33 +0200

    added asteroids splitting

--- src/main.c
+++ src/main.c
@@ -182,6 +182,59 @@ void asteroidMove (float dt) {
     }
 }
 
+void asteroidSplit (uint8_t id) {
+
+    float speedIncrease = 1.2;
+    float sizeDecrease = 0.75;
+
+    for (uint8_t i = 0; i < maxNumAsteroids; i++) {
+
+        if (!asteroids[i].radius) {
+
+            // inherit half radius
+            asteroids[i].radius = asteroids[id].radius * sizeDecrease;
+
+            // inherit same center
+            asteroids[i].center.x = asteroids[id].center.x;
+            asteroids[i].center.y = asteroids[id].center.y;
+
+            // same speed but offset by delta direction
+            uint16_t deltaDirection = randInt(20, 50);
+            float velX = asteroids[id].velocity.x*cos(deltaDirection / (180/M_PI)) - asteroids[id].velocity.y*sin(deltaDirection / (180/M_PI));
+            float velY = asteroids[id].velocity.y*cos(deltaDirection / (180/M_PI)) + asteroids[id].velocity.x*sin(deltaDirection / (180/M_PI));
+            asteroids[i].velocity.x = speedIncrease * velX;
+            asteroids[i].velocity.y = speedIncrease * velY;
+
+            // create the ships shape
+            for (uint8_t j = 0; j < asteroidCorners*2; j++) {
+                asteroids[i].relShape[j] = asteroids[id].relShape[j] * sizeDecrease;
+            }
+            // random rotation
+            rotate(asteroids[i].relShape, randInt(0, 360), asteroidCorners);
+
+            break;
+        }
+    }
+
+    // half the previous radius
+    asteroids[id].radius *= sizeDecrease;
+
+    // same speed but offset by delta direction
+    uint16_t deltaDirection = -1 * randInt(20, 50);
+    float velX = asteroids[id].velocity.x*cos(deltaDirection / (180/M_PI)) - asteroids[id].velocity.y*sin(deltaDirection / (180/M_PI));
+    float velY = asteroids[id].velocity.y*cos(deltaDirection / (180/M_PI)) + asteroids[id].velocity.x*sin(deltaDirection / (180/M_PI));
+    asteroids[id].velocity.x = speedIncrease * velX;
+    asteroids[id].velocity.y = speedIncrease * velY;
+
+    // create the ships shape
+    for (uint8_t j = 0; j < asteroidCorners*2; j++) {
+        asteroids[id].relShape[j] = asteroids[id].relShape[j] * sizeDecrease;
+    }
+    // random rotation
+    rotate(asteroids[id].relShape, randInt(0, 360), asteroidCorners);
+
+}
+
 void shoot () {
 
     for (uint8_t i = 0; i < maxNumShots; i++) {
@@ -241,8 +294,8 @@ void checkCollisions () {
                     <=  (asteroids[i].radius*asteroids[i].radius)) {
 
                         if (rayCastingCollision(shots[ii].shape, 1, asteroids[i].shape, asteroidCorners)) {
-                            // ADD THE DEATH (SPLIT) OF ASTEROIDS HERE
-                            asteroids[i].radius = 0;
+                            if (asteroids[i].radius > 12) asteroidSplit(i);
+                            else asteroids[i].radius = 0;
 
                             shots[ii].center.x = -1;
                             shots[ii].center.y = -1;
@@ -354,12 +407,12 @@ int main (void) {
 
     do {
 
-        while (timer_Get(TIMER) < ticksPerFrame) {}
+        while (timer_Get(TIMER) < ticksPerFrame) {}  // wait for 1/60 seconds to have passed
 
-        dt = timer_Get(TIMER) / ticksPerFrame;
-        timer_Set(TIMER, 0);
+        dt = timer_Get(TIMER) / ticksPerFrame;  // get factor for how much past said time it is
+        timer_Set(TIMER, 0);  // reset timer
 
-        kb_Scan();
+        kb_Scan();  // scan keyboard
         key = kb_Data[7];
 
         ship.acceleration = (key & kb_Up ? 0.6 : 0);
