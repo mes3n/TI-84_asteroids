2e834f345256b65a330042e5809cabb647ea20e2
mes3n <skidmackan@gmail.com>
2022-04-14 23:13:19 +0200

    added and stored highscore

---
+++ src/utilities/file.c
@@ -0,0 +1,14 @@
+#include "file.h"
+
+void saveVar (float var) {
+
+    real_t var_n = os_FloatToReal(var);
+    ti_SetVar(TI_REAL_TYPE, location, &var_n);
+}
+
+float readVar (void) {
+    void * var_n;
+
+    ti_RclVar(TI_REAL_TYPE, location, &var_n);
+    return os_RealToFloat(var_n);
+}
---
+++ src/utilities/file.h
@@ -0,0 +1,12 @@
+#ifndef FILE_H
+#define FILE_H
+
+#include <fileioc.h>
+#include <tice.h>
+
+char * location = ti_W;
+
+void saveVar (float);
+float readVar (void);
+
+#endif  // FILE_H
