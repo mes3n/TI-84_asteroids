6484e84b48cc4d3883ee40b34bf9a6c75a52fada
mes3n <skidmackan@gmail.com>
2022-04-15 12:35:21 +0200

    changed to colors

--- src/gfx/global_palette.c
+++ src/gfx/global_palette.c
@@ -2,5 +2,5 @@ unsigned char global_palette[6] =
 {
     0x00, 0x00, /*   0: rgb(  0,   0,   0) */
     0xff, 0xff, /*   1: rgb(255, 255, 255) */
-    0x7b, 0x6f, /*   2: rgb(222, 219, 222) */
+    0x18, 0xe3, /*   2: rgb(197, 198, 197) */
 };
